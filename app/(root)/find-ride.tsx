f8eb0952887abfdf15205987d578cc15fcaadc61
TariqKichawele <tariqkichawele66@gmail.com>
2025-01-28 13:40:55 -0500

    Add ride booking flow with bottom sheet and new screens
    - Integrate @gorhom/bottom-sheet for dynamic layout
    - Create confirm-ride and book-ride screens
    - Add DriverCard component for driver selection
    - Update RideLayout to support bottom sheet navigation
    - Enhance find-ride screen with location inputs and navigation

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,7 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useLocationStore } from "@/store";
-import { SafeAreaView } from "react-native-safe-area-context";
+import RideLayout from "@/components/RideLayout";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import { icons } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 
 const FindRide = () => {
   const {
@@ -11,10 +15,37 @@ const FindRide = () => {
     setUserLocation,
   } = useLocationStore();
   return (
-    <SafeAreaView>
-      <Text>FindRide: {userAddress}</Text>
-      <Text>FindRide: {destinationAddress}</Text>
-    </SafeAreaView>
+    <RideLayout title="Ride">
+      <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">From</Text>
+
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={userAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="#f5f5f5"
+          handlePress={(location) => setUserLocation(location)}
+        />
+      </View>
+
+      <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">To</Text>
+
+        <GoogleTextInput
+          icon={icons.map}
+          initialLocation={destinationAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="transparent"
+          handlePress={(location) => setDestinationLocation(location)}
+        />
+      </View>
+
+      <CustomButton
+        title="Find Now"
+        onPress={() => router.push(`/(root)/confirm-ride`)}
+        className="mt-5"
+      />
+    </RideLayout>
   );
 };
 
